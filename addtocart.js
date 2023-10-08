0d5092b8be791f5220166e9a46009602d03ddc24
Thai-FPT-Aptech <145465238+Thai-FPT-Aptech@users.noreply.github.com>
2023-10-08 17:10:03 +0700

    Add files via upload

---
+++ addtocart.js
@@ -0,0 +1,313 @@
+const product = [
+ {
+    "id": 1,
+    "title": "Panasonic CU/CS-XU12ZKH-8",
+    "price": 16740000,
+    "description": "Inverter air conditioner 1.5 HP ",
+    "brand": "Panasonic ",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303868/may-lanh-panasonic-inverter-15-hp-cu-cs-xu12zkh-81-1.jpg"
+}, {
+    "id": 2,
+    "title": "Panasonic CU/CS-PU9ZKH-8M",
+    "price": 10740000,
+    "description": "Inverter 1 HP",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303871/may-lanh-panasonic-inverter-1-hp-cu-cs-pu9zkh-81-1.jpg"
+}, {
+    "id": 3,
+    "title": "Panasonic CU/CS-PU12ZKH-8M",
+    "price": 13240000,
+    "description": "Inverter 1.5 HP ",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303872/may-lanh-panasonic-inverter-15-hp-cu-cs-pu12zkh-81-1.jpg"
+}, {
+    "id": 4,
+    "title": "Panasonic CU/CS-XU9ZKH-8",
+    "price": 13340000,
+    "description": "Inverter 1 HP ",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303867/may-lanh-panasonic-inverter-1-hp-cu-cs-xu9zkh-81-1.jpg"
+}, {
+    "id": 5,
+    "title": "Panasonic CU/CS-PU9ZKH-8M",
+    "price": 10740000,
+    "description": "Inverter 1 HP ",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303871/may-lanh-panasonic-inverter-1-hp-cu-cs-pu9zkh-81-1.jpg"
+}, {
+    "id": 6,
+    "title": "Panasonic CU/CS-XU9ZKH-8",
+    "price": 13340000,
+    "description": "Inverter 1 HP",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303867/may-lanh-panasonic-inverter-1-hp-cu-cs-xu9zkh-81-1.jpg"
+}, {
+    "id": 7,
+    "title": "Panasonic CU/CS-PU12ZKH-8M",
+    "price": 13240000,
+    "description": "Inverter 1.5 HP ",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303872/may-lanh-panasonic-inverter-15-hp-cu-cs-pu12zkh-81-1.jpg"
+}, {
+    "id": 8,
+    "title": "Panasonic CU/CS-XU12ZKH-8",
+    "price": 16740000,
+    "description": "Inverter 1.5 HP ",
+    "brand": "Panasonic",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/303868/may-lanh-panasonic-inverter-15-hp-cu-cs-xu12zkh-81-1.jpg"
+}, {
+    "id": 9,
+    "title": "Samsung AR10CYHAAWKNSV",
+    "price": 8990000,
+    "description": "Inverter 1 HP ",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/304226/may-lanh-samsung-inverter-15-hp-ar13cyfaawknsv1.jpg"
+}, {
+    "id": 10,
+    "title": "Samsung AR10CYFAAWKNSV",
+    "price": 9990000,
+    "description": "Inverter 1 HP ",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/304225/may-lanh-samsung-inverter-1-hp-ar10cyfaawknsv1-1.jpg"
+}, {
+    "id": 11,
+    "title": "Samsung AR12TYHQASINSV",
+    "price": 9390000,
+    "description": "Inverter 1.5 HP",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/309828/samsung-inverter-1-hp-ar12tyhqasinsv2.jpg"
+}, {
+    "id": 12,
+    "title": "Samsung Wind-Free AR13TYGCDWKNSV",
+    "price": 13390000,
+    "description": "Inverter 1.5 HP ",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/217448/samsung-ar13tygcdwknsv-1-1-org.jpg"
+}, {
+    "id": 13,
+    "title": "Samsung Wind-Free AR10TYGCDWKNSV",
+    "price": 13390000,
+    "description": "Inverter 1 HP",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/217447/samsung-ar10tygcdwknsv-1-3-org.jpg"
+}, {
+    "id": 14,
+    "title": "Samsung  AR18CYFAAWKNSV ",
+    "price": 20990000,
+    "description": "Inverter 2 HP",
+    "brand": " Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/217447/samsung-ar10tygcdwknsv-1-3-org.jpg"
+}, {
+    "id": 15,
+    "title": "Samsung AR13CYECAWKNSV",
+    "price": 14490000,
+    "description": "Inverter 1.5 HP ",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/309393/samsung-inverter-15-hp-ar13cyecawknsv2.jpg"
+}, {
+    "id": 16,
+    "title": "Samsung Wind-Free AR10TYGCDWKNSV ",
+    "price": 11390000,
+    "description": "Inverter 1 HP",
+    "brand": "Samsung",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/217447/samsung-ar10tygcdwknsv-1-3-org.jpg"
+}, {
+    "id": 17,
+    "title": "Nagakawa NIS-C09R2T28",
+    "price": 7140000,
+    "description": "Inverter 1 HP",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/299824/may-lanh-nagakawa-inverter-1-hp-nis-c09r2t281.jpg"
+}, {
+    "id": 18,
+    "title": "Nagakawa NIS-C12R2T28",
+    "price": 8490000,
+    "description": "Inverter 1.5 HP",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/299825/may-lanh-nagakawa-inverter-15-hp-nis-c12r2t281.jpg"
+}, {
+    "id": 19,
+    "title": "Nagakawa NIS-C12R2H10",
+    "price": 9420000,
+    "description": "Inverter 1.5 HP",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/235745/nagakawa-nis-c12r2h10-1-org.jpg"
+}, {
+    "id": 20,
+    "title": "Nagakawa NIS-C09R2H10",
+    "price": 8190000,
+    "description": "Inverter 1 HP",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/235744/nagakawa-nis-c09r2h10-1-org.jpg"
+}, {
+    "id": 21,
+    "title": "Nagakawa NIS-C24R2H10",
+    "price": 18990000,
+    "description": "Inverter 2.5 HP",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/299827/may-lanh-nagakawa-inverter-25-hp-nis-c24r2h101.jpg"
+}, {
+    "id": 22,
+    "title": "Nagakawa NT-C50R1T20",
+    "price": 42800000,
+    "description": "5 HP ceiling",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/309551/may-lanh-am-tran-nagakawa-5-hp-nt-1.jpg"
+}, {
+    "id": 23,
+    "title": "Nagakawa NT-C18R1T20",
+    "price": 21000000,
+    "description": "2 HP ceiling",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/309548/may-lanh-am-tran-nagakawa-2-hp-nt-1.jpg"
+}, {
+    "id": 24,
+    "title": "Nagakawa NT-C28R1T20",
+    "price": 27400000,
+    "description": "3 HP ceiling",
+    "brand": "Nagakawa",
+    "image": "https://cdn.tgdd.vn/Products/Images/2002/309549/may-lanh-am-tran-nagakawa-3-hp-nt-1.jpg"
+}, {
+    "id": 25,
+    "title": "Carrier 38/42 CER 013 Gas R410A",
+    "price": 9050000,
+    "description": "12,000BTU",
+    "brand": "Carrier",
+    "image": "https://s.meta.com.vn/Data/image/2021/03/31/may-lanh-carrier-38-42-cer-013-1.jpg"
+}, {
+    "id": 26,
+    "title": "Carrier 38/42 CER 013 12,000BTU Gas R410A",
+    "price": 8780000,
+    "description": "12,000BTU",
+    "brand": "Carrier",
+    "image": "https://s.meta.com.vn/Data/image/2021/03/31/may-lanh-carrier-38-42-cer-013-1.jpg"
+}, {
+    "id": 27,
+    "title": "Carrier Inverter 38/42 GCVBE 024 Gas R32",
+    "price": 18170000,
+    "description": "24,000BTU",
+    "brand": "Carrier",
+    "image": "https://s.meta.com.vn/Data/image/2021/04/07/may-lanh-carrier-inverter-38-42-gcvbe-024.jpg"
+}, {
+    "id": 28,
+    "title": "Carrier Inverter 38/42 GCVBE 013",
+    "price": 9450000,
+    "description": "12,000 BTU",
+    "brand": "Carrier",
+    "image": "https://s.meta.com.vn/Data/image/2021/05/29/dieu-hoa-carrier-inverter-38-42-gcvbe-013-1.jpg"
+}, {
+    "id": 29,
+    "title": "Carrier 38/42 CER 018 Gas R410A",
+    "price": 12620000,
+    "description": "18,000BTU",
+    "brand": "Carrier",
+    "image": "https://s.meta.com.vn/Data/image/2021/04/08/may-lanh-carrier-38-42-cer-018.jpg"
+}, {
+    "id": 30,
+    "title": "Carrier Inverter 38/42 GCVBE 010 Gas R32",
+    "price": 8030000,
+    "description": "9,000BTU",
+    "brand": "Carrier",
+    "image": "https://s.meta.com.vn/Data/image/2021/04/05/may-lanh-carrier-inverter-38-42-gcvbe-010.jpg"
+},
+///////////////////////   Another product //////////////////////////////////////////////////////////////////////////
+{
+    "id": 31,
+    "title": "Daikiosan DKA-04500A air conditioner fan",
+    "price": 3890000,
+    "image": "https://cdn.tgdd.vn/Products/Images/7498/218338/may-lam-mat-khong-khi-daikiosan-dka-04500a-1-2-org.jpg"
+}, {
+    "id": 32,
+    "title": "Senko 5-blade box fan BD230 28W",
+    "price": 240000,
+    "image": "https://cdn.tgdd.vn/Products/Images/1992/268446/hop-senko-bd230-1.jpg"
+}, {
+    "id": 33,
+    "title": "Sunhouse SHD7727 air conditioning fan",
+    "price": 3990000,
+    "image": "https://cdn.tgdd.vn/Products/Images/7498/199117/sunhouse-shd7727-kg-1-org.jpg"
+}, {
+    "id": 33,
+    "title": "Comfee 3-blade fan CF-FF4019VEW 48W",
+    "price": 450000,
+    "image": "https://cdn.tgdd.vn/Products/Images/1992/218463/quat-lung-comfee-cf-ff4019vew-trang-1-1-org.jpg"
+}, {
+    "id": 34,
+    "title": "Midea AC200-19WV air conditioner fan",
+    "price": 1990000,
+    "image": "https://cdn.tgdd.vn/Products/Images/7498/218307/quat-dieu-hoa-midea-ac200-19wv-1-1-org.jpg"
+}, {
+    "id": 35,
+    "title": "Senko 3-blade table fan B1213 40W",
+    "price": 350000,
+    "image": "https://cdn.tgdd.vn/Products/Images/1992/268448/ban-senko-b1213-1.jpg"
+}, {
+    "id": 36,
+    "title": "Sunhouse SHD7746 air conditioning fan",
+    "price": 4990000,
+    "image": "https://cdn.tgdd.vn/Products/Images/7498/114839/sunhouse-shd7746-1-org.jpg"
+}, {
+    "id": 37,
+    "title": "Senko 3-blade wall fan T1680 47W",
+    "price": 450000,
+    "image": "https://cdn.tgdd.vn/Products/Images/1992/268460/treo-senko-t1680-1-1.jpg"
+}, 
+];
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+
+    
+}
